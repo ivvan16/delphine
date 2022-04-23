9508b66f0eedad8ed5980ff2a7e20c29b5192604
ivvan16 <iatokarev1@mail.ru>
2022-04-23 12:38:46 +0300

    Mobile version is deployed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, HostListener } from '@angular/core';
+import { fromEvent } from "rxjs";
+import { debounceTime, map, startWith, tap } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 
 @Component({
@@ -60,9 +62,86 @@ export class AppComponent {
     }
   ];
 
+  teamMobileMembers = [
+    {
+      name: 'Dmitry',
+      role: 'Project Manager',
+      position: {
+        left: '1rem',
+        top: '8rem'
+      }
+    },
+    {
+      name: 'Diana',
+      role: 'Designer',
+      position: {
+        left: '1rem',
+        bottom: '8rem'
+      }
+    },
+    {
+      name: 'John',
+      role: 'Frontend Developer',
+      position: {
+        left: '16rem',
+        top: '0rem'
+      }
+    },
+    {
+      name: 'Denis',
+      role: 'Blockchain Developer',
+      position: {
+        right: '1rem',
+        top: '8rem'
+      }
+    },
+    {
+      name: 'Kirill',
+      role: 'Backend Developer',
+      position: {
+        right: '1rem',
+        bottom: '8rem'
+      }
+    },
+    {
+      name: 'Vadim',
+      role: 'Backend Developer',
+      position: {
+        left: '16rem',
+        bottom: '0rem'
+      }
+    }
+  ];
+
+  factsData = [
+    {
+      position: {top: '10rem', left: '8rem'},
+      text: 'NEAR’s fast (1 to 2 seconds) & budget-friendly transactions'
+    },
+    {
+      position: {top: '0rem', left: '25rem'},
+      text: 'Arts saved securely in Arweave'
+    },
+    {
+      position: {bottom: '1rem', left: '34rem'},
+      text: 'Marketplace fee at 5% per transaction'
+    }
+  ];
+
   assetsPath = environment.assetsPath;
 
   scroll(el: HTMLElement) {
     el.scrollIntoView({behavior: 'smooth'});
+  }
+
+  isMobile = fromEvent(window, 'resize')
+    .pipe(
+      debounceTime(100),
+      startWith({ target: { innerWidth: window.innerWidth }}),
+      map((e: any) => e.target.innerWidth < 500),
+    );
+
+  constructor(private cd: ChangeDetectorRef) {
+
   }
 }
